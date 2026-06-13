9db766a4007999832e65e41744fbf7e433e11845
IgorVinogradov1 <igor.vinogradov1@yandex.com>
2026-06-13 03:38:18 +0300

    split books list by pages

--- render_website.py
+++ render_website.py
@@ -1,3 +1,4 @@
+import os
 import json
 from livereload import Server
 from dotenv import load_dotenv
@@ -8,10 +9,10 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 def get_book_catalog():
     with open("meta_data.json", "r", encoding='utf-8') as my_file:
         books_json = my_file.read()
-    books_catalog = json.loads(books_json)
-    return list(chunked(books_catalog, len(books_catalog) // 2))
+    books_data = json.loads(books_json)
+    return list(chunked(books_data, 20))
 
-def on_reload():
+def on_reload(books_catalog):
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
@@ -19,18 +20,19 @@ def on_reload():
 
     template = env.get_template('template.html')
 
-    rendered_page = template.render(
-        books = get_book_catalog(),
-    )
-
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
-    print("index.html обновлен!")
+    for index, books_chunk in enumerate(books_catalog, start=1):
+        rendered_page = template.render(
+            books = books_chunk,
+        )
+        with open(f'pages/index{index}.html', 'w', encoding="utf8") as file:
+            file.write(rendered_page)
+        print(f"index{index}.html обновлен!")
 
 def main():
     load_dotenv()
-
-    on_reload()
+    os.makedirs('pages', mode=0o755, exist_ok=True)
+    books_catalog = get_book_catalog()
+    on_reload(books_catalog)
     server = Server()
     server.watch('template.html', on_reload)
     server.serve(root='.')
