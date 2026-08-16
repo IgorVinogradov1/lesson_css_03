ba9a969400f25269d8b0e16a052bc842205d6e0d
IgorVinogradov1 <igor.vinogradov1@yandex.com>
2026-08-17 02:28:28 +0300

    Update render_website.py

--- render_website.py
+++ render_website.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 import json
 from livereload import Server
 from dotenv import load_dotenv
@@ -6,8 +7,8 @@ from more_itertools import chunked
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-def get_book_catalog(books_per_page):
-    with open('meta_data.json', 'r', encoding='utf-8') as my_file:
+def get_book_catalog(books_per_page, file_path):
+    with open(file_path, 'r', encoding='utf-8') as my_file:
         books_data = json.load(my_file)
     return list(chunked(books_data, books_per_page))
 
@@ -32,8 +33,21 @@ def on_reload(books_catalog):
 def main():
     load_dotenv()
     os.makedirs('pages', mode=0o755, exist_ok=True)
-    books_per_page = 20
-    books_catalog = get_book_catalog(books_per_page)
+
+    parser = argparse.ArgumentParser(description='Загрузка пользовательского файла с данными')
+    parser.add_argument('--file', '-f', nargs='?', default='meta_data.json', help='Укажите путь к файлу (по умолчанию: meta_data.json)')
+    parser.add_argument('--books-per-page', '-p', type=int, default=20, help='Укажите количество книг на странице (по умолчанию: 20)')
+    args = parser.parse_args()
+    file_path = args.file
+    books_per_page = args.books_per_page
+    
+    try:
+        books_catalog = get_book_catalog(books_per_page, file_path)
+    except FileNotFoundError:
+        parser.error(f'файл с данными не найден')
+    except json.JSONDecodeError as error:
+        parser.error(f'некорректный json-файл')
+
     on_reload(books_catalog)
     server = Server()
     server.watch('template.html', lambda: on_reload(books_catalog))
